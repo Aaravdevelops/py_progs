e83c676173dab908efdd9e87ce9013efeb877cc3
Aarav655765 <aaravofficial9954@gmail.com>
2024-04-10 17:52:55 +0000

    p_o_v0.0.0.0.0.2 updated in the repo

--- p_o_v0.0.0.0.0.2.py
+++ p_o_v0.0.0.0.0.2.py
@@ -13,6 +13,50 @@ if (a=="y"):
             file.write("\n")
             file.write("___________________________________________________________________________________________________________________________________")
         print("the data was entered successfully!")
+        n=input("do you want to put some more data? ")
+        if (n=="y"):
+            d = input("do you have a yaahoo account? ")
+        if (d=="y"):
+            e = input("enter the yaaho email address: ")
+            ee = getpass.getpass("enter the yaahoo password")
+            with open("passes.txt",'w') as file:
+                file.write(e)
+                file.write("\n")
+                file.write(ee)
+                file.write("\n")
+                file.write("_______________________________________________________________________________________________________________________________")
+                print("the data was entered successfully")
+                
+        elif (d=="n"):
+            f = input("do you have a microsoft account? ")
+            if (f=="y"):
+                g = input("enter the microsoft email address:")
+                gg = getpass.getpass("enter the password")
+                with open("passes.txt",'w') as file:
+                    file.write(g)
+                    file.write("\n")
+                    file.write(gg)
+                    file.write("\n")
+                    file.write("___________________________________________________________________________________________________________________________")
+                    print("the data was entered successfully")
+                    
+            elif (f=="n"):
+                h = input("do you have a amazon account? ")
+                if (h=="y"):
+                    m =  input("enter the amazon account address: ")
+                    mm = getpass.getpass("enter the amazon account password")
+                    with open("passes.txt",'w') as file:
+                        file.write(m)
+                        file.write("\n")
+                        file.write(mm)
+                        file.write("\n")
+                        file.write("_______________________________________________________________________________________________________________________")
+                        print("the data was entered successfully")
+                        
+                else:
+                    print("sorry, i did'nt get that.")
+            else:
+                print("sorry, i did'nt get that.")
     elif (b =="n"):
         d = input("do you have a yaahoo account? ")
         if (d=="y"):
