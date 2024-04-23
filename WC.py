d34645b876156636051c367f53be3c4b34d056b0
Aarav655765 <aaravofficial9954@gmail.com>
2024-04-23 12:16:52 +0000

    WC.py added

---
+++ WC.py
@@ -0,0 +1,4 @@
+import os
+print("Hello, this is python and I am going to count the number of lines present in your desired file name..........")
+filen=input("Please Enter the file name:")
+os.system("wc -w ",filen)
