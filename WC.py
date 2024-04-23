e2593af79593fc02b47b772ee4abe121736197c8
Aarav655765 <aaravofficial9954@gmail.com>
2024-04-23 12:47:03 +0000

    WC.py Upgraded lvl. 1

--- WC.py
+++ WC.py
@@ -2,6 +2,7 @@ import os
 print("Hello, this is python and I am going to count the number of lines present in your desired file name..........")
 filen=input("Please Enter the file name<file extension should be included>:")
 if os.path.isfile(filen):
+    os.system("ls")
     os.system(f"wc -w '{filen}'.")
 else:
     print("It's a CAP,File does not exist")
