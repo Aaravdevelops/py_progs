c8f6cd58e132c29639743359c16d3f0edc4e63e4
Aarav655765 <aaravofficial9954@gmail.com>
2024-04-23 12:59:51 +0000

    WC.py Upgraded lvl. 1

--- WC.py
+++ WC.py
@@ -1,4 +1,13 @@
 import os
-print("Hello, this is python and I am going to count the number of lines present in your desired file name..........")
-filen=input("Please Enter the file name<file extension should be included>:")
-os.system(f"wc -w '{filen}'.")
+
+print("Hello, this is Python and I am going to count the number of lines present in your desired file name...")
+
+filen = input("Please Enter the file name <file extension should be included>: ")
+
+try:
+    if os.path.isfile(filen):
+        os.system(f"wc -w '{filen}'")
+    else:
+        print("File does not exist.")
+except Exception as e:
+    print("An error occurred:", e)
